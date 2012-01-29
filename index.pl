e93569deb8c56b519390ac11d0fb6f739116a99e
St Ts <tsaneff@gmail.com>
2012-01-30 01:22:14 +0200

    Simple web UI with Mojolicious...

---
+++ index.pl
@@ -0,0 +1,6 @@
+#!C:/perl/bin/perl.exe
+use Mojolicious::Lite;
+# NB: 'warning' and 'strict' are automatically enabled with 'Mojolicious::Lite'.
+
+# Uncomment, when this code is on production: $ENV{MOJO_MODE} = 'production'; 
+require 'main.pl';
