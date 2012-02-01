a329c21b7b30fd15c21e9f9b041cb97c82a0ee10
St Ts <tsaneff@gmail.com>
2012-02-02 00:22:03 +0200

    small midnight changes...

---
+++ index.pl
@@ -0,0 +1,7 @@
+﻿#!C:/perl/bin/perl.exe
+use Mojolicious::Lite;
+# NB: 'warning' and 'strict' are automatically enabled with 'Mojolicious::Lite'.
+
+# Uncomment below line, when this code is on production:
+# $ENV{MOJO_MODE} = 'production'; 
+require 'main.pl';
